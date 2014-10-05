235b47b3ff3564a013790e41d5cfd1f541dd4c37
ag15divy <ag15divy@users.noreply.github.com>
2014-10-04 18:36:11 -0700

    Create BallBrickBreaker.java

---
+++ BallBrickBreaker.java
@@ -0,0 +1,54 @@
+package BrickBreaker;
+import zen.core.Zen;
+public class Ball {
+
+	
+		int x;
+		int y;
+		int dx;
+		int dy;
+		
+		
+	
+	public Ball(){
+		x = 240;
+		y = 700;
+		dx = Zen.getRandomNumber(-5, 5);
+		dy = Zen.getRandomNumber(-8, -5);
+		
+		
+	}
+
+	public void draw() {
+		Zen.setColor("black");
+		Zen.fillOval(x-10, y-10, 20, 20);
+		
+	}
+	public void move() {
+		x = x + dx;
+		y = y + dy;
+		
+		if (x>490){
+			x = 490;
+			dx = -dx;
+		}
+		if (x < 10){
+			x = 10;
+			dx = -dx;
+		}
+		if (y<10){
+			y = 10;
+			dy = -dy;
+			
+		}
+	
+}
+
+	public boolean isTouching(Paddle p) {
+		if (Math.abs(p.x - x) < 50 && Math.abs(p.y -y) < 20){
+			return true;
+		}
+		return false;
+	}
+	
+}
