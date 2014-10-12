e0bfd95e42e85b346f0aaa9ef9f0b891a0149a07
ag15divy <ag15divy@users.noreply.github.com>
2014-10-11 18:29:22 -0700

    Update BrickBreaker.java

--- BrickBreaker.java
+++ BrickBreaker.java
@@ -8,51 +8,67 @@ public class BrickBreaker {
 
 	public static void main(String[] args) {
 		Zen.create(500, 800);
-		
-		
+
 		Paddle p = new Paddle();
 		Ball b = new Ball();
 		ArrayList<Brick> bricks = new ArrayList<Brick>();
-		
+
 		int brickX = 50;
-		while (brickX<500){
-			Brick br = new Brick(brickX, 200);
+		while (brickX < 500) {
+			Brick br = new Brick(brickX, 300);
 			bricks.add(br);
-			brickX= brickX + 50;
-			
+			brickX = brickX + 50;
+
+		}
+
+		int tbrickX = 50;
+		while (tbrickX < 500) {
+			TouchingBrick tbr = new TouchingBrick(tbrickX, 100);
+			bricks.add(tbr);
+			tbrickX = tbrickX + 50;
 		}
 		
-		while (true){
+		int mbrickX = 50;
+		while (mbrickX < 500) {
+			MovingBrick mbr = new MovingBrick(mbrickX, 200);
+			bricks.add(mbr);
+			mbrickX = mbrickX + 100;
+		}
+
+		while (true) {
 			drawBackground();
 			p.draw();
 			b.draw();
 			p.move();
 			b.move();
-			
-			for (Brick br: bricks){
-				br.draw();
-				
-			}
-			
-			if (b.isTouching(p)){
-				b.dy = -b.dy;
-				
-			}
-			if (b.isTouching(p)){
-				b.dy= -b.dy;
+
+			for (int i = 0; i < bricks.size(); i++) {
+				if (bricks.get(i).isTouching(b) && bricks.get(i).health <= 0) {
+					bricks.remove(i);
+					b.dy = -b.dy;
+					i--;
+				} else if (bricks.get(i).isTouching(b)) {
+					b.dy = -b.dy;
+				} else {
+					bricks.get(i).draw();
+					bricks.get(i).move();
+				}
+
 			}
 
-		Zen.buffer(33);
+			if (p.isTouching(b)) {
+				b.dy = -b.dy;
+
+			}
+
+			Zen.buffer(33);
 		}
-		
-		
-		
-		
+
 	}
 
 	private static void drawBackground() {
 		Zen.setBackground("white");
-		
+
 	}
 
 }
