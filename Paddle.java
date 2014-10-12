489f8f0c5e1e7223d9f14c7ee8842cd473adf37c
ag15divy <ag15divy@users.noreply.github.com>
2014-10-11 18:29:55 -0700

    Update Paddle.java

--- Paddle.java
+++ Paddle.java
@@ -1,18 +1,20 @@
 package BrickBreaker;
+
 import zen.core.Zen;
+
 public class Paddle {
-		int x;
-		int y;
-	public Paddle(){
+	int x;
+	int y;
+
+	public Paddle() {
 		x = 250;
 		y = 750;
 	}
-	
-	
+
 	public void draw() {
 		Zen.setColor("red");
-		Zen.fillRect(x-40, y, 80, 10);
-		
+		Zen.fillRect(x - 50, y, 100, 10);
+
 	}
 
 	public void move() {
@@ -22,6 +24,13 @@ public class Paddle {
 		if (Zen.isKeyPressed("left")){
 			x = x- 10;
 		}
-		 
+	}
+	public boolean isTouching(Ball b) {
+		if (Math.abs(b.x - x) < 55 && Math.abs(b.y - y) < 15) {
+			b.dx = Zen.getRandomNumber(-8, 8);
+			return true;
+		}
+		return false;
+
 	}
 }
