03aa6ec16c7c32b0b9e64df947ae672ac18b3d7e
ag15divy <ag15divy@users.noreply.github.com>
2014-10-11 18:28:40 -0700

    Update Brick.java

--- Brick.java
+++ Brick.java
@@ -5,14 +5,32 @@ import zen.core.Zen;
 public class Brick {
 	int x;
 	int y;
+	int health;
+	
+	public Brick(){
+		x = 200;
+		y = 200;
+		health = 1;
+	}
 	public Brick(int newX, int newY){
 		x = newX;
 		y = newY;
+		health = 1;
 		
 	}
 	public void draw(){
 		Zen.setColor("light blue");
 		Zen.fillRect(x- 20, y-10, 40, 20);
 	}
-	
+	public void move(){
+		
+	}
+	public boolean isTouching(Ball b){
+		if (Math.abs(x - b.x)< 25 && Math.abs(y - b.y)< 16){
+			health--;
+			b.dx = Zen.getRandomNumber(-8,8);
+			return true;
+		}
+		return false;
+	}
 }
