ec0c027020c8cae7553950a0a3c34d1be7ddd11b
ag15divy <ag15divy@users.noreply.github.com>
2014-10-25 18:30:37 -0700

    Create Coin.java

---
+++ Coin.java
@@ -0,0 +1,24 @@
+package FallingCoins;
+
+import zen.core.Zen;
+
+public class Coin {
+	public static isBrick;
+	int x, y, speed, value;
+	boolean isBrick;
+	
+	public Coin(){
+		x = Zen.getRandomNumber(20,595);
+		y = 5;
+		speed = 5;
+		value = 1;
+		isBrick = false;
+	}
+	public void draw(){
+		Zen.setColor("brown");
+		Zen.fillOval(x, y, 10, 10);
+	}
+	public void move(){
+		y +=speed;
+	}
+}
