ee39120e34484e760a823cd47b629427bfbb49b8
ag15divy <ag15divy@users.noreply.github.com>
2014-10-25 18:32:06 -0700

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,43 @@
+package FallingCoins;
+import zen.core.Zen;
+
+public class Player {
+	int x, y, dy;
+	boolean jumping;
+	public Player(){
+		x = 30;
+		y = 480;
+		dy = 0;
+	}
+	public void draw(){
+		Zen.setColor("black");
+		Zen.fillOval(x-20, y-20, 40, 40);
+	}
+	public void move(){
+		if(Zen.isKeyPressed("left")){
+		x-=10;
+	}
+	if (Zen.isKeyPressed("right")){
+		x+=10;
+	}
+	if (Zen.isKeyPressed("up")&&jumping == false){
+		dy = -25;
+		jumping = true;
+	}
+	if(y>480){
+		y = 480;
+		dy = 0;
+		jumping = false;
+	}
+	else{
+		y += dy;
+		dy++;
+	}
+}
+	public boolean collectsp(Coin coin) {
+		if(Math.abs(x-coin.x)<25&&Math.abs(y-coin.y)<25){
+			return true;
+		}
+		return false;
+	}
+}
