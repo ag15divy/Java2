0cc7689c346d5e89dc906c5cf2f9c3cc17eb3aeb
ag15divy <ag15divy@users.noreply.github.com>
2014-10-25 18:31:38 -0700

    Create Brick2.java

---
+++ Brick2.java
@@ -0,0 +1,17 @@
+package FallingCoins;
+import zen.core.Zen;
+public class Brick extends Coin{
+
+public Brick() {
+	x = Zen.getRandomNumber(5,595);
+	y = 10;
+	speed = 7;
+	value = 0;
+	isBrick = true;
+}
+	public void draw(){
+		Zen.setColor("red");
+		Zen.fillRect(x, y, 30, 20);
+	}
+}
+
