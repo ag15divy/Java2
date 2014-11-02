2f6b6b4a32e3b7ac86422eb012fbcffb59ff965d
ag15divy <ag15divy@users.noreply.github.com>
2014-11-01 18:41:18 -0700

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,19 @@
+package Snake;
+
+import zen.core.Zen;
+
+public class Food {
+	int x;
+	int y;
+	public void showAt(int i, int j){
+		x = i;
+		y = j;
+	}
+	public void draw(){
+		Zen.setColor("red");
+		Zen.fillOval(x, y, 10, 10);
+		
+	}
+
+}
+
